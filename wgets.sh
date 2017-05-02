bbdedb064f135b800735e4b1f3ba08b476eb72ba
root <Jon Bardin>
2017-05-02 03:00:45 -0700

    better grub and rootfs priming

---
+++ wgets.sh
@@ -0,0 +1,3 @@
+wget http://archive.ubuntu.com/ubuntu/dists/xenial-updates/main/installer-amd64/current/images/hwe-netboot/mini.iso
+wget http://releases.ubuntu.com/xenial/ubuntu-16.04.2-server-amd64.iso
+wget http://security.ubuntu.com/ubuntu/pool/main/l/linux-hwe/linux-image-extra-4.8.0-36-generic_4.8.0-36.36~16.04.1_amd64.deb
