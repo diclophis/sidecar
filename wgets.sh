b8f9fd651ded286b4b8bda715f80988421a0e0d9
Mavenlink Provisioning User <diclophis@gmail.com>
2018-05-19 23:39:31 -0700

    use kernel version that matches installer boot kernel for extra mods

--- wgets.sh
+++ wgets.sh
@@ -10,6 +10,6 @@ then
   cd tmp
   wget http://archive.ubuntu.com/ubuntu/dists/xenial-updates/main/installer-amd64/current/images/hwe-netboot/mini.iso
   wget http://releases.ubuntu.com/xenial/ubuntu-16.04.4-server-amd64.iso
-  wget http://security.ubuntu.com/ubuntu/pool/main/l/linux-hwe/linux-image-extra-4.8.0-58-generic_4.8.0-58.63~16.04.1_i386.deb
+  wget http://security.ubuntu.com/ubuntu/pool/main/l/linux-hwe/linux-image-extra-4.8.0-36-generic_4.8.0-36.36~16.04.1_amd64.deb
   touch got-wgets
 fi
