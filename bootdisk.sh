9dbe04f964d9fa4c26199fa662ffc9f2ae1fe3eb
Jon Bardin <diclophis@gmail.com>
2018-06-14 10:24:10 -0700

    fix args to bootup

--- bootdisk.sh
+++ bootdisk.sh
@@ -9,12 +9,6 @@ set -x
 
 BOOTDISK_DEV=${1}
 
-if [ -z "${BOOTDISK_DEV}" ];
-then
-  echo bad BOOTDISK_DEV arg
-  exit 1
-fi
-
 sh ~/sidecar/wgets.sh
 sh ~/sidecar/apt.sh
 
@@ -24,33 +18,36 @@ cd tmp
 sudo umount ubuntu-16.04.4-server-amd64.iso server-iso || true
 sudo umount mini.iso || true
 
-# sudo umount ${BOOTDISK_DEV}1 || true
-# sudo umount ${BOOTDISK_DEV}2 || true
-# sudo umount ${BOOTDISK_DEV}3 || true
-# sudo sgdisk --zap-all ${BOOTDISK_DEV}
-# sudo sgdisk -n 1:2048:4095 -c 1:"BIOS Boot Partition" -t 1:ef02 ${BOOTDISK_DEV}
-# sudo sgdisk -n 2:4096:8191 -c 2:"Linux /boot" -t 2:8300 ${BOOTDISK_DEV}
-# ENDSECTOR=`sudo sgdisk -E ${BOOTDISK_DEV}`
-# sudo sgdisk -n 3:8192:${ENDSECTOR} -c 3:"EFI System Partition" -t 3:ef00 ${BOOTDISK_DEV}
-# sudo umount ${BOOTDISK_DEV}1 || true
-# sudo umount ${BOOTDISK_DEV}2 || true
-# sudo umount ${BOOTDISK_DEV}3 || true
-# sudo mkfs.vfat -v -F32 -n GRUB2EFI ${BOOTDISK_DEV}3
-# sudo rm -Rf new-iso
-# mkdir -p new-iso
-# sudo mount -t vfat ${BOOTDISK_DEV}3 new-iso -o uid=1000,gid=1000,umask=022
+if [ -n "${BOOTDISK_DEV}" ];
+then
+  sudo umount ${BOOTDISK_DEV}1 || true
+  sudo umount ${BOOTDISK_DEV}2 || true
+  sudo umount ${BOOTDISK_DEV}3 || true
+  sudo sgdisk --zap-all ${BOOTDISK_DEV}
+  sudo sgdisk -n 1:2048:4095 -c 1:"BIOS Boot Partition" -t 1:ef02 ${BOOTDISK_DEV}
+  sudo sgdisk -n 2:4096:8191 -c 2:"Linux /boot" -t 2:8300 ${BOOTDISK_DEV}
+  ENDSECTOR=`sudo sgdisk -E ${BOOTDISK_DEV}`
+  sudo sgdisk -n 3:8192:${ENDSECTOR} -c 3:"EFI System Partition" -t 3:ef00 ${BOOTDISK_DEV}
+  sudo umount ${BOOTDISK_DEV}1 || true
+  sudo umount ${BOOTDISK_DEV}2 || true
+  sudo umount ${BOOTDISK_DEV}3 || true
+  sudo mkfs.vfat -v -F32 -n GRUB2EFI ${BOOTDISK_DEV}3
+  sudo rm -Rf new-iso
+  mkdir -p new-iso
+  sudo mount -t vfat ${BOOTDISK_DEV}3 new-iso -o uid=1000,gid=1000,umask=022
+fi
 
 mkdir -p new-iso-copy
 
 # TODO: cache rebake via re-layering from other isos
-# mkdir -p extras
-# for I in linux-image-*
-# do
-#   dpkg -x $I extras
-# done
-# cd extras
-# find . | cpio --quiet --dereference -o -H newc | gzip -9 > ../extras.gz
-# cd ..
+mkdir -p extras
+for I in linux-image-*
+do
+  dpkg -x $I extras
+done
+cd extras
+find . | cpio --quiet --dereference -o -H newc | gzip -9 > ../extras.gz
+cd ..
 
 mkdir -p kickseeds
 cp ../workstation-install.cfg kickseeds
@@ -96,11 +93,14 @@ cp -R new-iso-copy/* new-iso
 
 sync -f new-iso/boot/grub/grub.cfg
 
-# ## install boot loader
-# ##sudo parted ${BOOTDISK_DEV} set 1 bios_grub on
-# ##sudo parted ${BOOTDISK_DEV} set 1 boot on
-# sudo grub-install --removable --boot-directory=new-iso/boot --efi-directory=new-iso/EFI/BOOT ${BOOTDISK_DEV} || true
-# 
-# sudo umount ${BOOTDISK_DEV}1 || true
-# sudo umount ${BOOTDISK_DEV}2 || true
-# sudo umount ${BOOTDISK_DEV}3 || true
+if [ -n "${BOOTDISK_DEV}" ];
+then
+  ## install boot loader
+  ##sudo parted ${BOOTDISK_DEV} set 1 bios_grub on
+  ##sudo parted ${BOOTDISK_DEV} set 1 boot on
+  sudo grub-install --removable --boot-directory=new-iso/boot --efi-directory=new-iso/EFI/BOOT ${BOOTDISK_DEV} || true
+
+  sudo umount ${BOOTDISK_DEV}1 || true
+  sudo umount ${BOOTDISK_DEV}2 || true
+  sudo umount ${BOOTDISK_DEV}3 || true
+fi;
